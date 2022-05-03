0f656cd4117e120ee824e6dc0704b526386ad2aa
Diego Escobedo <diego@escobedo.com>
2022-05-03 17:00:22 -0400

    added some barebones code files to start building structure

---
+++ main.py
@@ -0,0 +1,17 @@
+""" main.py
+
+Handles the training of the Reinforcement Learning Agent for the Wiki Game
+
+"""
+
+from parse import parser
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    main(args)
---
+++ parse.py
@@ -0,0 +1,5 @@
+import argparse
+
+parser = argparse.ArgumentParser("WALDO - Wikipedia Agent for Learning Definition-informed Objectives")
+
+parser.add_argument("--save", type=str, default="../results/tmp")
