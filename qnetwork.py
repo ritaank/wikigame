69c413f100f2a229ff1d46a534562c892d065629
Diego Escobedo <diego@escobedo.com>
2022-05-03 17:21:52 -0400

    added basic skeleton for Q Network, Transition, and Replay Buffer code

---
+++ qnetwork.py
@@ -0,0 +1,33 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class QNetwork(nn.Module):
+    """ Actor (Policy) Model."""
+    def __init__(self, state_size, action_size, seed, fc1_units=64,  fc2_units= 64):
+        """
+        Initialize parameters and build model.
+        Params
+        =======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_unit (int): Number of nodes in first hidden layer
+            fc2_unit (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork,self).__init__() ## calls __init__ method of nn.Module class
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        
+    def forward(self,x):
+        # x = state
+        """
+        Build a network that maps state -> action values.
+        """
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        out = self.fc3(x)
+        return out
